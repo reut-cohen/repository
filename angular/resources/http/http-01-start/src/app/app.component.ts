ba63ef76ebf6d3cf0e84c11398d68a5fd5a734c8
Reut Cohen <46457595+reut-cohen@users.noreply.github.com>
2021-05-03 21:11:35 +0300

    http request

---
+++ angular/resources/http/http-01-start/src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators'
+import { Post } from './post.module';
+import { PostsService } from './post.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
+  private errorSub: Subscription;
+
+  constructor(private http: HttpClient,
+    private postsService: PostsService) { }
+
+  ngOnInit() {
+    this.fetchPosts();
+    this.errorSub = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    })
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.postsService.createAndStorePost(postData.title, postData.content);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+
+  onClearPosts() {
+    // Send Http request
+    this.postsService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
+  }
+
+  private fetchPosts() {
+    this.isFetching = true;
+    this.postsService.fetchPosts().
+      subscribe(
+        (posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+        error => {
+        this.isFetching = false;
+        console.log(error)
+       this.error = error.message;
+    });
+
+  }
+
+}
